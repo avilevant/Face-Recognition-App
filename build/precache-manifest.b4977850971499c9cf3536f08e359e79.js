6caf3928cb2d44c2748e87f45e271f739142ca57
avilevant <53899685+avilevant@users.noreply.github.com>
2020-03-08 18:37:54 +0200

    Added a Procfile.

---
+++ build/precache-manifest.b4977850971499c9cf3536f08e359e79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca9c341ccf079b397aa165cac32c318",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fde93761bef4a5b771c",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "fe029dc2e391524b3ecf",
+    "url": "/static/css/main.cd384c93.chunk.css"
+  },
+  {
+    "revision": "6fde93761bef4a5b771c",
+    "url": "/static/js/2.96bfe4e3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.96bfe4e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe029dc2e391524b3ecf",
+    "url": "/static/js/main.cd26c4a2.chunk.js"
+  },
+  {
+    "revision": "02179e477418cf452016",
+    "url": "/static/js/runtime-main.0c718081.js"
+  }
+]);
