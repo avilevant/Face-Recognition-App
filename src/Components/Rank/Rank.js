600aa54b20bede66b6bc41fa7199ac8f7ed875b2
avilevant <53899685+avilevant@users.noreply.github.com>
2020-03-09 21:43:22 +0200

    changes

--- src/Components/Rank/Rank.js
+++ src/Components/Rank/Rank.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
 const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
-      </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-    </div>
-  );
+    return ( <
+        div >
+        <
+        div className = 'white f2' > { `${name}, the number of photos you had entered is...` } <
+        /div> <
+        div className = 'white f1' > { entries } <
+        /div> < /
+        div >
+    );
 }
 
 export default Rank;
